5c93640fb1c7065a8567c708f2c7841cb87bded1
yiersan <kharkavin.vd@gmail.com>
2021-06-20 13:42:59 +0000

    add constants

--- Resource.py
+++ Resource.py
@@ -1,8 +1,9 @@
 import os
 import pygame.image
+from Game1 import CELL_WIDTH, CELL_HEIGHT
 
 
 Working_directory = os.path.dirname(__file__)
 Assets = os.path.join(Working_directory, 'assets')
 ball = pygame.image.load(os.path.join(Assets, 'ball.png'))
-ball = pygame.transform.scale(ball, (60, 60))
+ball = pygame.transform.scale(ball, (CELL_WIDTH, CELL_HEIGHT))
